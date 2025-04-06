e6d300d724e20bf2264ed122555dcc0abed47850
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-05 22:04:33 -0700

    View scroll try out

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, Image, ImageBackground,ScrollView} from "react-native";
+const logo = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60}}>
+        <ScrollView>
+        <Image source={logo} style={{width: 300, height: 300}}/>
+        <Image source={{uri: 'https://picsum.photos/200'}} style={{width: 300, height: 300}}/>
+  
+
+        <ImageBackground source={logo} style={{ flex:1, width: 300, height: 300}}>
+        <Text style={{color: "white"}}>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+    
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+    
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+    
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+    
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+        </Text>
+        </ImageBackground>
+
+        <Image source={logo} style={{width: 300, height: 300}}/>
+        <Image source={{uri: 'https://picsum.photos/200'}} style={{width: 300, height: 300}}/>
+        </ScrollView>
+    </View> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
