8db5ff4eb5d9bc88a74be35320c99d826e63fe8f
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-05 22:33:42 -0700

    Button styling

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, TouchableOpacity, Text} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex : 1, backgroundColor: "plum", padding: 60}}>
+      <TouchableOpacity 
+        style={{
+          backgroundColor: 'white',
+          padding: 10,
+          borderRadius: 5,
+          alignItems: 'center'
+        }}
+        onPress={() => console.log("Button pressed")}
+      >
+        <Text style={{ color: 'black' }}>Click me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
