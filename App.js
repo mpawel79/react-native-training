dca5f2e14fc2c195264e9e62ec0140f2e7e579c1
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-08 22:10:30 -0700

    Pressable component on Image

--- App.js
+++ App.js
@@ -14,7 +14,9 @@ export default function App() {
       >
         <Text style={{ color: 'black' }}>Click me</Text>
       </TouchableOpacity>
-      <Pressable onPress={()=> console.log("Button pressed")}>
+      <Pressable onPress={()=> console.log("Button pressed")} 
+                 onLongPress={()=> console.log("Button long pressed")}
+      style={{backgroundColor: "white", padding: 10, borderRadius: 5, alignItems: "center"}}>
       <Image source={require("./assets/adaptive-icon.png")} style={{width: 100, height: 100}} />
       </Pressable>
     </View>
