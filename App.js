8ac4045d8442d4dab6841ebb53608718e36a3d45
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-09 20:16:11 -0700

    Modal view shown on button

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, TouchableOpacity, Text, Image, Modal, Button} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex : 1, backgroundColor: "plum", padding: 60}}>
+      <TouchableOpacity 
+        style={{
+          backgroundColor: 'white',
+          padding: 10,
+          borderRadius: 5,
+          alignItems: 'center'
+        }}
+        onPress={() => {console.log("Button pressed"); setIsModalVisible(true)}}
+      >
+        <Text style={{ color: 'black' }}>Click me</Text>
+      </TouchableOpacity>
+      <Modal visible={isModalVisible} animationType="slide" 
+            onRequestClose={() => {console.log("Modal closed"); setIsModalVisible(false)}}
+            presentationStyle="pageSheet"
+            >
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" , padding: 30}}>
+            <Text>Hello</Text>
+            <Button title="Close" onPress={() => {console.log("Button pressed closing"); setIsModalVisible(false)}} />
+        </View>
+
+
+      </Modal>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
