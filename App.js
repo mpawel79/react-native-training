353d0af94ddd1cc4c172c61556908b39528ca47b
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-09 20:22:20 -0700

    Animating

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ActivityIndicator} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ActivityIndicator size="large" color="plum" animating={false}/>
+      <ActivityIndicator size="small" color="plum" animating={true}/>
+      <ActivityIndicator size="large" color="plum" animating={false}/>
+      <ActivityIndicator size="small" color="plum" animating={false}/>
+      <ActivityIndicator size="large" color="plum" animating={false}/>
+      <ActivityIndicator size="small" color="plum" animating={false}/>
+      <ActivityIndicator size="large" color="plum" animating={false}/>
+      <ActivityIndicator size="small" color="plum" animating={false}/>
     </View>
   );
 }
