abf8ca28b43a8536d10ea62ec1a6fb3852e82a85
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-09 20:19:27 -0700

    Hidded

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor="plum" barStyle="dark-content" hidden={false}/>
     </View>
   );
 }
