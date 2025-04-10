bf4cbe92f07456dfb9699dd30c5adc5be37e7727
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-09 21:14:59 -0700

    3 alerts

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Alert 1" onPress={() => Alert.alert("Button pressed")} />
+     <Button title="Alert 2" onPress={() => Alert.alert("Button pressed", "This is an alert")} />
+     <Button title="Alert 3" onPress={() => Alert.alert("Button pressed", "This is an alert", [
+      {text: "Cancel", onPress: () => console.log("Cancel Pressed")},
+      {text: "OK", onPress: () => console.log("OK Pressed")},
+     ])} />
     </View>
   );
 }
