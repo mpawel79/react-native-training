b8b6116a30a693782a01758705b6343448cb69b3
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-10 21:15:24 -0700

    Shadows styling

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-        <View style={[styles.box, styles.lightblueBg]}>
-            <Text style={{borderRadius: 10, borderWidth: 2, borderColor: 'black', borderStyle: 'dashed', padding: 10, fontSize: 20, fontWeight: 'bold', color: 'white'}}>Lightblue box</Text>
+        <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
+            <Text style={{borderRadius: 10, borderWidth: 2, borderColor: 'green', borderStyle: 'dashed', padding: 10, fontSize: 20, fontWeight: 'bold', color: 'white'}}>Lightblue box</Text>
         </View>
-        <View style={[styles.box, styles.lightgreenBb]}>
+        <View style={[styles.box, styles.lightgreenBb, styles.androidShadow]}>
             <Text>Lightgreen box</Text>
         </View>
     </View>
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   box: {
-    width: "100%",
+    width: "50%",
     height: "25%",
     padding: 20,
     borderColor: 'black',
@@ -37,5 +37,22 @@ const styles = StyleSheet.create({
   },
   lightgreenBb: {
     backgroundColor: 'lightgreen',
-  }
+  },
+  boxShadow: {
+    shadowColor: 'blue',
+    shadowOffset: {width: 10, height: 12},
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  shadowOpacity: {
+    shadowOpacity: 0.5,
+  },
+  shadowRadius: {
+    shadowRadius: 10,
+  },
+  androidShadow: {
+    elevation: 10,
+    shadowColor: 'blue',
+  },
+  
 });
