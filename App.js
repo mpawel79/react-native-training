4282c9dc0cb2e2ec8284d0bfa30a9bf75e5092bf
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-10 20:22:32 -0700

    styles array

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Greet from "./components/Greet";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Greet name="John" />     
-      <Greet name="Jane" />
-      <Greet name="Jim" />  
+        <View style={[styles.box, styles.lightblueBg]}>
+            <Text>Lightblue box</Text>
+        </View>
+        <View style={[styles.box, styles.lightgreenBb]}>
+            <Text>Lightgreen box</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 60,
   },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 20,
+  },
+  lightblueBg: {
+    backgroundColor: 'lightblue',
+  },
+  lightgreenBb: {
+    backgroundColor: 'lightgreen',
+  }
 });
