f3acc96c1745cbd12ca94f5e18f485abdcb04e14
Pawel Mergist <pawel@MacBook-Pro.local>
2025-04-19 21:08:34 -0700

    alignItems

--- app/index.tsx
+++ app/index.tsx
@@ -4,16 +4,16 @@ import Box from '../components/Box';
 export default function App() {
     return (
         <View style={styles.container}>
-            <Box style={{ backgroundColor: 'olive',  }}>Box 1</Box>
-            <Box style={{ backgroundColor: 'brown',  }}>Box 2</Box>
-            <Box style={{ backgroundColor: 'darkslategray',  }}>Box 3</Box>
-            {/* <Box style={{ backgroundColor: 'yellow' }}>Box 4</Box>
-            <Box style={{ backgroundColor: 'purple' }}>Box 5</Box>
-            <Box style={{ backgroundColor: 'orange' }}>Box 6</Box>
+            <Box style={{ backgroundColor: 'olive', alignSelf: 'flex-start' }}>Box 1</Box>
+            <Box style={{ backgroundColor: 'brown',  alignSelf: 'flex-end'}}>Box 2</Box>
+            <Box style={{ backgroundColor: 'darkslategray', alignSelf: 'center' }}>Box 3</Box>
+            <Box style={{ backgroundColor: 'yellow' , alignSelf: 'stretch'}}>Box 4</Box>
+            <Box style={{ backgroundColor: 'purple', alignSelf: 'auto' }}>Box 5</Box>
+            <Box style={{ backgroundColor: 'orange', alignSelf:  "flex-end" }}>Box 6</Box>
             <Box style={{ backgroundColor: 'pink' }}>Box 7</Box>
             <Box style={{ backgroundColor: 'gray' }}>Box 8</Box>
             <Box style={{ backgroundColor: 'brown' }}>Box 9</Box>
-            <Box style={{ backgroundColor: 'black' }}>Box 10</Box> */}
+            <Box style={{ backgroundColor: 'black' }}>Box 10</Box>
         </View>
     )
 }
@@ -21,8 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        flexDirection: 'row-reverse',
+        alignItems: 'flex-start',
         borderWidth: 6,
         borderColor: 'red',
     },
