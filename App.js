873a67e1a20c009b19b134b0e2c6c98cd035378a
pawelppi <pawel.mergist@perpetual-intelligence.com>
2025-05-14 21:52:02 -0500

    keyboardavoidingview

--- App.js
+++ App.js
@@ -1,35 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet, StatusBar ,SafeAreaView,
-  TextInput,Switch} from "react-native";
+  TextInput,Button, Image, KeyboardAvoidingView} from "react-native";
 import { useState } from "react";
 
 const App = () => {
-  const [name, setName] = useState("inital value");
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello World </Text>
-  
-      <TextInput style={styles.multiline} 
-      value={name} onChangeText={setName} 
-      placeholder="Enter your name" 
-      secureTextEntry={false}
-      keyboardType="email-address"
-      autoCapitalize="none"
-      autoCorrect={false}
-      multiline={true}
-      numberOfLines={4}
-      />
-      <Text style={styles.text}>{name}</Text>
-      <Switch value={isEnabled} 
-      onValueChange={setIsEnabled}
-      trackColor={{
-        true: "#000",
-        false: "#000",
-      }}
-      thumbColor={isEnabled ? "#000" : "#fff"}
-      />
-    </SafeAreaView>
+    <KeyboardAvoidingView behavior="padding" 
+    style={styles.container}
+    keyboardVerticalOffset={Platform.OS === "ios" ? 50 : -200}
+    >
+      <View style={styles.form}>
+      <Image source={require('./assets/adaptive-icon.png')} style={styles.image} />
+      <Text style={styles.text}>User Login</Text>
+      <TextInput style={styles.input} placeholder="Enter your name" value={name} onChangeText={setName}   />
+      <TextInput style={styles.input} placeholder="Enter your email" value={email} onChangeText={setEmail} />
+      <TextInput style={styles.input} placeholder="Enter your password" 
+      secureTextEntry={true} value={password} 
+      onChangeText={setPassword} />
+      <Button title="Login" onPress={() => {}} style={styles.button} />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -56,6 +50,42 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     borderRadius: 5,
     padding: 10,
+    margin: 10,
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    margin: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 15,
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+  },
+  button: {
+    backgroundColor: "#000",
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+  },
+  image: {
+    alignSelf: "center",
+    marginBottom: 20,
+    width: 100,
+    height: 100,
     margin: 10,
   },
 });
