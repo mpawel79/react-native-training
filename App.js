4e367df2eab3987ecbb0c6f9ce9a1eda89827f64
mpawel79 <m.pawel79@gmail.com>
2025-05-23 23:48:53 -0500

    added stacked screen

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import { Ionicons } from '@expo/vector-icons';
+import AppStack from './AppStack';
 
 const Tab = createBottomTabNavigator();
+
+const commonOptions = {
+  headerShown: true,
+  tabBarActiveTintColor: 'red',
+  tabBarLabelStyle: { fontSize: 12, fontWeight: 'bold' },
+  tabBarLabelPosition: 'beside-icon',
+  tabBarStyle: { backgroundColor: 'blue' }
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ 
-        tabBarLabelPosition: "beside-icon", 
-     
-        tabBarActiveTintColor : 'blue'}}>
-        <Tab.Screen name="Home" component={HomeScreen} 
-        options={{ headerShown: false, tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />, tabBarActiveTintColor: 'red' ,
-        tabBarIcon: ()=> <Ionicons name="home" color="red" size={24} />}} />
-        <Tab.Screen  name="Settings" component={SettingsScreen} 
-        options={{ tabBarBadge: 3, tabBarBadgeStyle: { backgroundColor: 'red', color: 'white' },  headerShown: false, tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} /> }} />
+      <Tab.Navigator screenOptions={commonOptions}>
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            tabBarIcon: () => <Ionicons name="home" color="red" size={24} />,
+            tabBarLabel: 'Home'
+          }} 
+        />
+        <Tab.Screen 
+          name="Settings" 
+          component={SettingsScreen} 
+          options={{
+            tabBarIcon: ({ color, size }) => <Ionicons name="settings" color={color} size={size} />,
+            tabBarBadge: 3,
+            tabBarBadgeStyle: { backgroundColor: 'red', color: 'white' }
+          }} 
+        />
+        <Tab.Screen 
+          name="About Stack" 
+          component={AppStack} 
+          options={{
+            tabBarIcon: () => <Ionicons name="information-circle" color="red" size={24} />,
+            tabBarLabel: 'About'
+          }} 
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
